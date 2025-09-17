7df1ef20c575b6b773b05019208002e43cce250a
Vineet-os1 <k250117@nu.edu.pk>
2025-09-17 23:23:15 +0500

    Create Task5.c

---
+++ Task5.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // Display student details using escape sequences
+    printf("Name: Ali\n");
+    printf("Age: 20\n");
+    printf("Course: \"C Programming\"\n");
+    printf("Result: Passed\n");
+    
+    return 0;
+}
